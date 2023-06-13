1fd6664676af0f858b8b5a2240a876b2caa93d8c
ALEX <ALEX@DESKTOP-AP66AE1>
2023-06-13 01:12:55 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFacturacion/FactFacturaCabecera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFacturacion
+{
+    public class FactFacturaCabecera
+    {
+        [Key]
+        public int IdFacturaCabecera { get; set; }
+
+        public DateTime? FechaFactura { get; set; }
+
+        public double? Subtotal { get; set; }
+
+        public double? Iva { get; set; }
+
+        public double? Total { get; set; }
+
+        public bool? Estado { get; set; }
+
+        public string? NumeroFactura { get; set; }
+
+        public string? IdentificacionCliente { get; set; }
+
+        public int? IdTipo { get; set; }
+
+        public virtual FactCliente? Cliente { get; set; }
+
+        public virtual ICollection<FactDetalleFactura> DetallesFactura { get; } = new List<FactDetalleFactura>();
+
+        public virtual FactTipoPago? TipoPago { get; set; }
+    }
+}
