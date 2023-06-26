17a625fe65f8a1128b4460cdb9d9337026642ce0
EDWIN ALEXANDER CADENA CHAVARRIA <eacadenac@utn.edu.ec>
2023-06-26 03:55:22 -0500

    jejem

--- Facturacion.UAPI/Crud.cs
+++ Facturacion.UAPI/Crud.cs
@@ -1,7 +1,153 @@
-﻿namespace Facturacion.UAPI
+﻿using System.Net;
+
+namespace Facturacion.UAPI
 {
-    public class Class1
+    public class Crud<T>
     {
+        public T[] Select(string Url)
+        {
+
+            try
+            {
+                using (var api = new webClient())
+                {
+                    api.headers.add("content-type", "application/json");
+                    var json = api.Downloadstring(Url);
+                    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<T[]>(json);
+                    return data;
+
+
+                }
+
+            }
+            catch (WebException ex)
+            {
+                throw new Exception("Ha sucedido un error inesperado (" + ex.Message + ")");
+
+            }
+        }
+
+        public T SelectByID(string Url, string id)
+        {
+
+            try
+            {
+
+                //por id
+                using (var api = new webClient())
+                {
+                    api.headers.add("content-type", "application/json");
+                    var json = api.Downloadstring(Url + "/id" + id);
+                    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+                    return data;
+
+
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new Exception("Ha sucedido un error inesperado (" + ex.Message + ")");
+
+            }
+        }
+
+
+
+        public void Udate(string Url, T data, string id)
+        {
+
+            {
+
+                using (var api = new webClient())
+                {
+                    api.headers.add("content-type", "application/json");
+                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                    api.UploadString(Url + "/" + id, "PUT", json);
+                }
+                try
+            }
+                catch (WebException ex)
+                {
+                    throw new Exception("Ha sucedido un error inesperado (" + ex.Message + ")");
+
+                }
+
+            }
+
+            public T Insert(string Url, T data)
+            {
+
+
+
+                try
+                {
+                    using (var api = new webClient())
+                    {
+                        api.headers.add("content-type", "application/json");
+                        var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                        json = api.UploadString(Url, "POST", json);
+                        data = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+                        return data;
+                    }
+
+
+                }
+                catch (WebException ex)
+                {
+                    throw new Exception("Ha sucedido un error inesperado (" + ex.Message + ")");
+
+                }
+
+
+
+
+
+
+
+
+            }
+
+            public void Delete(string Url, string id)
+            {
+                //trycatch al momento de borrar
+                try
+                {
+                    using (var api = new webClient())
+                    {
+                        api.headers.add("content-type", "application/json");
+                        api.UploadString(Url + "/" + id, "DELETE", "");
+                    }
+
+                }
+                catch (WebException ex)
+                {
+                    throw new Exception("Ha sucedido un error inesperado (" + ex.Message + ")");
+
+                }
+            }
+
+
+
+
+
+
+
+
+        }
 
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
